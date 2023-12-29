2139b6ebea486ac594e2e4e8408d230ace2bf550
L-Karimi <luciahkei101@gmail.com>
2023-12-29 03:23:04 +0300

    modified readme

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  })
+
+  /** @type {import('next').NextConfig} */
+  const path = require('path')
+
+  const nextConfig = {
+    reactStrictMode: true,
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
+  }
+
+  module.exports = nextConfig
